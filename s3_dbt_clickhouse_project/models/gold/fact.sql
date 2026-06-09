4d2024c56d7dd01b2f0811f0edfe34124d2711b2
anbion <anbion@yandex.ru>
2026-06-09 21:31:57 +0300

    The hard-coded table source has been replaced with refs in fact.sql and obt.sql

--- s3_dbt_clickhouse_project/models/gold/fact.sql
+++ s3_dbt_clickhouse_project/models/gold/fact.sql
@@ -1,23 +1,23 @@
 {% set configs = [
     {
-            "table": "de_pet_project_db.gold.obt"
+            "model": ref("obt")
             , "columns" : "gold_obt.booking_id, gold_obt.listing_id, gold_obt.host_id, gold_obt.total_booking_amount, gold_obt.service_fee, gold_obt.cleaning_fee, gold_obt.accommodates, gold_obt.bedrooms, gold_obt.bathrooms, gold_obt.price_per_night, gold_obt.response_rate"
             , "alias" : "gold_obt"
     }, 
     {
-            "table": "de_pet_project_db.gold.dim_listings"
+            "model": ref("dim_listings")
             , "columns" : ""
             , "alias" : "gl"
             , "join_condition" : "gl.listing_id = gold_obt.listing_id"
     }, 
     {
-            "table": "de_pet_project_db.gold.dim_hosts"
+            "model": ref("dim_hosts")
             , "columns" : ""
             , "alias" : "gh"
             , "join_condition" : "gold_obt.host_id = gh.host_id"
     }, 
     {
-            "table": "de_pet_project_db.gold.dim_bookings"
+            "model": ref("dim_bookings")
             , "columns" : ""
             , "alias" : "gb"
             , "join_condition" : "gold_obt.booking_id = gb.booking_id"
@@ -29,9 +29,9 @@ SELECT
 FROM
     {% for config in configs %}
         {% if loop.first %}
-            {{ config['table'] }} AS {{ config['alias'] }}
+            {{ config['model'] }} AS {{ config['alias'] }}
         {% else %}
-            LEFT JOIN {{ config['table'] }} AS {{ config['alias'] }}
+            LEFT JOIN {{ config['model'] }} AS {{ config['alias'] }}
             ON {{ config['join_condition'] }}
         {% endif%}
     {% endfor %}
--- s3_dbt_clickhouse_project/models/gold/obt.sql
+++ s3_dbt_clickhouse_project/models/gold/obt.sql
@@ -1,17 +1,17 @@
 {% set configs = [
     {
-            "table": "de_pet_project_db.silver.silver_bookings"
+            "model": ref("silver_bookings")
             , "columns" : "sb.*"
             , "alias" : "sb"
     }, 
     {
-            "table": "de_pet_project_db.silver.silver_listings"
+            "model": ref("silver_listings")
             , "columns" : "sl.host_id, sl.property_type, sl.room_type, sl.city, sl.country, sl.accommodates, sl.bedrooms, sl.bathrooms, sl.price_per_night, sl.price_per_night_tag, sl.created_at AS listings_created_at"
             , "alias" : "sl"
             , "join_condition" : "sb.listing_id = sl.listing_id"
     }, 
     {
-            "table": "de_pet_project_db.silver.silver_hosts"
+            "model": ref("silver_hosts")
             , "columns" : "sh.host_name, sh.host_since, sh.is_superhost, sh.response_rate, sh.response_rate_quality, sh.created_at AS host_created_at"
             , "alias" : "sh"
             , "join_condition" : "sl.host_id = sh.host_id"
@@ -25,9 +25,9 @@ SELECT
 FROM
     {% for config in configs %}
         {% if loop.first %}
-            {{ config['table'] }} AS {{ config['alias'] }}
+            {{ config['model'] }} AS {{ config['alias'] }}
         {% else %}
-            LEFT JOIN {{ config['table'] }} AS {{ config['alias'] }}
+            LEFT JOIN {{ config['model'] }} AS {{ config['alias'] }}
             ON {{ config['join_condition'] }}
         {% endif%}
     {% endfor %}
